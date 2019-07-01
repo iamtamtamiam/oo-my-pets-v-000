eb6d07a28c7db7074afc080060c99deb0abd4de9
Tam Vu <vutam_a@hotmail.com>
2019-07-01 16:23:57 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -75,7 +75,7 @@ class Owner
     self.dogs.clear
     self.cats.clear
     
-    dogs.each {|dog| puts dog.owner}
+    dogs.each {|dog| print dog.owner}
     
     
     #self.cats.each {|cat| cat.owner.clear}
