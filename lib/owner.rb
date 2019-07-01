2da268e226a32a2d31ddb83b9d7e4416b54d81c3
Tam Vu <vutam_a@hotmail.com>
2019-07-01 16:13:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -70,7 +70,7 @@ class Owner
     self.cats.each {|kitty| kitty.mood = "nervous"}
     self.dogs.each {|dog| dog.mood = "nervous"}
     
-    puts dog.owner
+    print dog.owner
     
     #self.dogs.clear
     #self.cats.clear
