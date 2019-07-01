79f038338c5be7330f3a0b09cb7260f03351147d
Tam Vu <vutam_a@hotmail.com>
2019-07-01 16:21:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 class Dog
   # code goes here 
   attr_reader :name
@@ -6,7 +6,7 @@ class Dog
   
   @@all = [] 
   
-  binding.pry
+
   def initialize(name, owner)
     @name = name
     @mood = 'nervous'
